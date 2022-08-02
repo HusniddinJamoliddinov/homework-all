7b63b48ba8d41b7103d87d87b85adcf1ebea6e3c
Husniddin <jamoliddinovhusniddin7212@gmail.com>
2022-08-02 10:46:16 +0500

    add new codes

--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export class Calculator extends Component {
   render() {
     return (
-      <div>Calculator1</div>
+      <div>Calculatorghvgvvjbj</div>
     )
   }
 }
