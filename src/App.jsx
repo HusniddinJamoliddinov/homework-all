6cd8852359e6ddbfdba19c8607dcce90f7890b9a
Husniddin <jamoliddinovhusniddin7212@gmail.com>
2022-08-11 07:59:49 +0500

    tic-tac start codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,12 @@
-import React, { Component } from "react";
-import Table from "./components/table";
-import "./app.scss";
+import './App.css';
+import TicTacToe from "./components/ticTactToe/ticTacToe";
 
-class App extends Component {
-  theadLength = (columnCount) => {
-    const tableHead = [];
-    for (let i = 1; i < columnCount; i++) {
-      tableHead.push(<th>Column {i}</th>);
-    }
-    tableHead.push(<th>Action</th>);
-    return tableHead;
-  };
-
-  tbodyLength = (rowCount) => {
-    const tableBody = [];
-    for (let i = 0; i < rowCount; i++) {
-      tableBody.push(
-        <tr>
-          <td>Husniddin {i}</td>
-          <td>23</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>London Park no {i}</td>
-          <td>Action</td>
-        </tr>
-      );
-
-    }
-    return tableBody;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Table
-          theadLength={this.theadLength}
-          tbodyLength={this.tbodyLength}
-        />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe/>
+    </div>
+  );
 }
 
 export default App;
